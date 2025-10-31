30145c71196a227ff9170c9e3d300c2f6b4852b6
Relizene <vovaegorov689@gmail.com>
2025-10-31 11:04:32 +0300

    add dotenv

--- app/settings.py
+++ app/settings.py
@@ -11,13 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-<<<<<<< HEAD
-=======
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
->>>>>>> c37841f (Light fix for endpoint on fastapi)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
